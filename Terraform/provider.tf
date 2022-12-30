48eaed756124823ed3c3226f998ae5c114e2cda0
Janarthanan Kugathasan <jana19930918@gmail.com>
2022-12-31 01:03:28 +0800

    updated terraform backend provider

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region  = "ap-southeast-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "gitaction-go-api-sqs"
+    key    = "gitaction"
+    region  = "ap-southeast-1"
+  }
+}
